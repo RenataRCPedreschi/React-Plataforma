a5c546e2ceefa39445181c0e9f39411f9757ea79
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-03-21 14:03:00 -0300

    rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import { Menu } from "./components/Menu/Menu";
-import { Home } from './pages/Home/Home';
-import { Blog } from './pages/Blog/Blog';
-import { Contato } from './pages/Contato/Contato';
-import { Usuarios } from './pages/Usuarios/Usuarios';
-import { Login } from './pages/Login/Login';
-
+import { Home } from "./pages/Home/Home";
+import { Blog } from "./pages/Blog/Blog";
+import { Contato } from "./pages/Contato/Contato";
+import { Usuarios } from "./pages/Usuarios/Usuarios";
+import { Login } from "./pages/Login/Login";
+import { NotFound } from "./pages/NotFound/NotFound";
+import { Root } from "./pages/Root/Root";
+import { Section1 } from "./pages/Section1/Section1";
+import { Section2 } from "./pages/Section2/Section2";
+import { Section3 } from "./pages/Section3/Section3";
 
 function App() {
   return (
     <div className="App">
-      
       <BrowserRouter>
-        <Menu />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contato" element={<Contato />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Root />}>
+            <Route path="/" element={<Home />}>
+              <Route path="/section1" element={<Section1 />} />
+              <Route path="/section2" element={<Section2 />} />
+              <Route path="/section3" element={<Section3 />} />
+            </Route>
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/usuarios" element={<Usuarios />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
