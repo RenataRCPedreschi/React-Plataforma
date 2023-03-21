3159f84d0862bebdd751ca6499f72851fd56b640
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-03-21 14:38:12 -0300

    formulário login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Menu } from "./components/Menu/Menu";
+import { Home } from './pages/Home/Home';
+import { Blog } from './pages/Blog/Blog';
+import { Contato } from './pages/Contato/Contato';
+import { Usuarios } from './pages/Usuarios/Usuarios';
+import { Login } from './pages/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+      
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/usuarios" element={<Usuarios />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
