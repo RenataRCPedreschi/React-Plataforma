7b2d3b50d26e3fdaa74ca93ec42a30d5ef63eb63
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-03-21 14:52:04 -0300

    atualizado

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,7 +6,7 @@ import "./Login.css"
 export function Login() {
   return (
     <Container className="containerForm">
-    <Form className='form' >
+    <Form className='form'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Digite seu email" />
@@ -14,21 +14,25 @@ export function Login() {
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3 " controlId="formBasicPassword">
         <Form.Label>Senha</Form.Label>
         <Form.Control type="password" placeholder="Digite sua senha" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Mantenha-me logado" />
+        <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
+      <Container className='btn1'>
       <Button variant="primary" type="submit">
         Entrar
       </Button>
+      </Container>
+      <Container className='btn2'>
       <Button variant="primary" type="submit">
       <i class="bi bi-google"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
   <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
 </svg> Entrar com o Google</i>
       </Button>
+      </Container>
     </Form>
     </Container>
   );
